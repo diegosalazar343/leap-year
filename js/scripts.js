0a5944a0fca6bb809f5aba412325760969cf43b3
diegosalazar343 <salazardiego343@gmail.com>
2020-08-19 11:02:47 -0700

    fix scripts so that there are no errors
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
+/* $(document).ready(function() {
+  $("#leapYear").submit(function(event) {
+  event.preventDefault;
+  let yearOutput= parseInt($("#yearInput").val());
+ function isLeapYear(year) {
+      if ((yearOutput % 4 === 0) && (yearOutput % 100 !== 0) || (yearOutput % 400 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+
+
+  console.log(yearOutput);
+}); */
+
+
 $(document).ready(function() {
-  const year = parseInt(prompt("Please type in a year to check if it is a leap year:"));
-}
+  $("#leapYear").submit(function(event) {
+  event.preventDefault();
+
+    let yearOutput = parseInt($("#yearInput").val());
+
+console.log(yearOutput);
+    function isLeapYear(year) {
+      if ((yearOutput % 4 === 0) && (yearOutput % 100 !== 0) || (yearOutput % 400 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    console.log(isLeapYear(2020))
+  });
+});
