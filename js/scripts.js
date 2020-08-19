6fb50f7ca337190f13cb4c4e1cc3e027b2b13b2c
diegosalazar343 <salazardiego343@gmail.com>
2020-08-19 13:35:01 -0700

    reveal false or true answers to input
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,16 +18,25 @@ $(document).ready(function() {
   $("#leapYear").submit(function(event) {
   event.preventDefault();
 
-    let yearOutput = parseInt($("#yearInput").val());
+    let year = parseInt($("#yearInput").val());
 
-console.log(yearOutput);
+console.log(year);
     function isLeapYear(year) {
-      if ((yearOutput % 4 === 0) && (yearOutput % 100 !== 0) || (yearOutput % 400 === 0)) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+
+        $("#leapTrue").show();
+
         return true;
       } else {
+        $("#leapFalse").show();
         return false;
+
       }
+
     };
-    console.log(isLeapYear(2020))
+    $("#year").text(isLeapYear(year));
+    $("#leapResult").show();
+    $("#leapYear").hide();
+
   });
 });
